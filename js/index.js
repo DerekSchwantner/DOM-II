9297e01afdfc99b178cb1577b304bd6e3d7dd10d
Derek S <dschwan21@me.com>
2019-05-14 15:15:24 -0700

    more events added, js/index.js

--- js/index.js
+++ js/index.js
@@ -8,31 +8,50 @@ navItems[3].addEventListener("dblclick", event => {
   copyright.classList.toggle("hidden");
 });
 
+navItems.forEach(a =>
+  a.addEventListener("click", event => {
+    event.target.style.color = "orange";
+  })
+);
+navItems.forEach(a =>
+  a.addEventListener("mouseup", event => {
+    event.target.style.fontSize = "2rem";
+  })
+);
+
+//Text Content
+const txtContent = document.querySelectorAll("p");
+txtContent[3].addEventListener("dblclick", event => {
+  event.target.style.fontSize = "2rem";
+});
+
+txtContent[0].addEventListener("mouseover", event => {
+  event.target.style.color = "blue";
+});
+
 //Buttons
 const buttons = document.querySelectorAll(".content-pick .destination .btn");
 
-// buttons[0].addEventListener("click", function() {
-//   console.log("I was clicked");
-// });
-buttons[1].addEventListener("dblclick", function() {
-  console.log("I was double clicked");
-});
-// buttons[2].addEventListener("click", function() {
-//   console.log("I was clicked");
-// });
 buttons.forEach(div =>
   div.addEventListener("click", function() {
     console.log("I was clicked");
   })
 );
 
-//
-const midTitle = document.querySelector(".content-destination h2");
-function logSelection(event) {
-  const selection = event.target.value.substring(
-    event.target.selectionStart,
-    event.target.selectionEnd
-  );
-  console.log(`You selected: ${selection}`);
-}
-midTitle.addEventListener("select", logSelection(event));
+buttons[1].addEventListener("dblclick", function(event) {
+  console.log("I was double clicked", event);
+});
+buttons[2].addEventListener("click", function(event) {
+  event.target.style.color = "green";
+});
+
+//Titles
+// const midTitle = document.querySelector(".content-destination h2");
+// function logSelection(event) {
+//   const selection = event.target.value.substring(
+//     event.target.selectionStart,
+//     event.target.selectionEnd
+//   );
+//   console.log(`You selected: ${selection}`);
+// }
+// midTitle.addEventListener("select", logSelection(event));
